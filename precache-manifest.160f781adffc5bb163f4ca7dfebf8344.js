30c8baf44df7ce12004afd96c1eb1a24b564f4fd
Aashish Maharjan <aashishmaharjan2001@gmail.com>
2020-08-23 00:47:28 +0545

    Updates

---
+++ precache-manifest.160f781adffc5bb163f4ca7dfebf8344.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9f2f0742e974efe7b487129b189df85",
+    "url": "/covid19tracker/index.html"
+  },
+  {
+    "revision": "53cc29a3d54faca1bd66",
+    "url": "/covid19tracker/static/css/main.547dacf5.chunk.css"
+  },
+  {
+    "revision": "4097df57c5f81b0a5bbd",
+    "url": "/covid19tracker/static/js/2.7de45890.chunk.js"
+  },
+  {
+    "revision": "c46ade285a7bd9e73d4de71c9509bd70",
+    "url": "/covid19tracker/static/js/2.7de45890.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53cc29a3d54faca1bd66",
+    "url": "/covid19tracker/static/js/main.24081908.chunk.js"
+  },
+  {
+    "revision": "b202280f02d6149b08df",
+    "url": "/covid19tracker/static/js/runtime-main.5fe113ed.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19tracker/static/media/logo.d7265326.png"
+  }
+]);
